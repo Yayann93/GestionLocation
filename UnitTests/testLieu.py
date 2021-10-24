ae4c2d9e5664370d163d0918e31520205ee42f50
Pier4413 <pierre.delmas@delmasweb.net>
2021-10-24 11:30:26 +0200

    Ajout du début des tests unitaires

---
+++ UnitTests/testLieu.py
@@ -0,0 +1,13 @@
+import unittest
+from lieu import Lieu
+
+class TestLieu(unittest.TestCase):
+
+    def test_str(self):
+        l = Lieu(0,"Bahamas",12)
+        d = l.__str__()
+        self.assertEquals(d, "code du lieu de location : 0; lieu de la location : Bahamas; prix à la journée : 12")
+    
+    def test_getPrix(self):
+        l = Lieu(0,"Bahamas",12)
+        self.assertEquals(l.getPrix(), 12)
