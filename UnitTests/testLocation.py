07da1ff88266d21cc66458079f1d400844cc5cfa
Pier4413 <Pier4413@users.noreply.github.com>
2021-10-28 13:45:54 +0200

    Correction espace en plus

--- UnitTests/testLocation.py
+++ UnitTests/testLocation.py
@@ -6,4 +6,4 @@ class TestLocation(unittest.TestCase):
     def test_str(self):
         s = Louer(250, 500,"Bahamas", "Auchan", "Durant")
         d = s.__str__()
-        self.assertEquals(d, "Date de début de location : 250 ; Date de fin de location : 500 ; Salle louée : Bahamas ; Organisme locataire : Auchan ; Client : Durant")
+        self.assertEquals(d, "Date de début de location : 250; Date de fin de location : 500; Salle louée : Bahamas; Organisme locataire : Auchan; Client : Durant")
