60c364ed7580db390c074e536d3242d05b3a1fc8
Yann <yannmarchand59@gmail.com>
2021-10-28 12:58:21 +0200

    Changement dans les fichiers et classes

---
+++ UnitTests/testLocation.py
@@ -0,0 +1,9 @@
+import unittest
+from location import Salle, Louer
+
+class TestLocation(unittest.TestCase):
+
+    def test_str(self):
+        s = Louer(12/5/2020, 16/5/2020,"Bahamas", "Auchan", "Durant")
+        d = s.__str__()
+        self.assertEquals(d, "Date de début de location : 12/06/2020 ; Date de fin de location : 16/06/2020 ; Salle louée : Bahamas ; Organisme locataire : Auchan ; Client : Durant")
--- classes/organisme.py
+++ classes/organisme.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:classes/organisme.py
 class Organisme:
     def __init__(self, codeOrga, nomOrga, numOrga) -> None:
         """
@@ -28,3 +29,89 @@ class Organisme:
         return self.__nomOrga
     def getnumOrga (self) -> int:
         return self.__numOrga
+=======
+class Organisme:
+
+    def __init__(self, codeOrganisme, nomOrganisme, numAgrement) -> None:
+
+        """
+        
+        Constructeur Organisme
+        :param codeOrganisme: son code dans la base de donnée
+        :param numOrganisme: son numéro d'agrément
+        
+        """
+
+        self.setcodeOrganisme (codeOrganisme)
+        self.setnomOrganisme (nomOrganisme)
+        self.setnumAgrement (numAgrement)
+
+    
+    #Setter
+
+    def setcodeOrganisme (self, codeOrganisme : int) -> None:
+        self.__codeOrganisme = codeOrganisme
+
+    def setnomOrganisme (self, nomOrganisme : str) -> None:
+        self.__nomOrganisme = nomOrganisme
+
+    def setnumAgrement (self, numAgrement : int) -> None:
+        self.__numAgrement = numAgrement
+
+    #Getter
+
+    def getcodeOrganisme (self) -> int:
+        return self.__codeOrganisme
+
+    def getnomOrganisme (self) -> str:
+        return self.__nomOrganisme
+
+    def getnumAgrement (self) -> int:
+        return self.__numAgrement
+            
+class Client:
+
+    def __init__(self, codeClient, nomClient, adresseClient, dateDeNaissance, organisme) -> None:
+        """
+        Constructeur de Client
+        :param codeClient: Code du client dans la base de donnée
+        """
+        self.setcodeClient (codeClient)
+        self.setnomClient (nomClient)
+        self.setadresseClient (adresseClient)
+        self.setdateDeNaissance (dateDeNaissance)
+        self.setorganisme (organisme)
+
+    #Setter
+    def setcodeClient (self, codeClient : int) -> None:
+        self.__codeClient = codeClient
+
+    def setnomClient (self, nomClient : int) -> None:
+        self.__nomClient = nomClient
+
+    def setadresseClient (self, adresseClient : str) -> None:
+        self.__adresseClient = adresseClient
+
+    def setdateDeNaissance (self, dateDeNaissance : int) -> None:
+        self.__dateDeNaissance = dateDeNaissance
+
+    def setorganisme (self, organisme : str) -> None:
+        self.__organisme = organisme
+
+
+    #Getter
+    def getcodeClient (self) -> int:
+        return self.__codeClient
+
+    def getnomClient (self) -> int:
+        return self.__nomClient
+
+    def getadresseClient (self) -> str:
+        return self.__adresseClient
+
+    def getdateDeNaissance (self) -> int:
+        return self.__dateDeNaissance
+
+    def getorganisme (self) -> str:
+        return self.__organisme
+>>>>>>> Stashed changes:organisme.py
