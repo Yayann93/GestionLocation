1f8258d64f47b1662abc240af99f7694ec1189d4
Pier4413 <pierre.delmas@delmasweb.net>
2021-11-12 16:11:34 +0100

    Correction d'un petit probleme sur le test de organisme

--- UnitTests/testOrganisme.py
+++ UnitTests/testOrganisme.py
@@ -6,4 +6,4 @@ class TestOrganisme(unittest.TestCase):
     def test_str(self):
         o = Organisme(2, "Auchan",51)
         d = o.__str__()
-        self.assertEquals(d, "Code Organisme : 2; Nom Organisme : Auchan; Numéro d'Agrément : 51")
+        self.assertEquals(d, "Code Organisme :2; Nom Organisme :Auchan; Numéro d'Agrément :51")
